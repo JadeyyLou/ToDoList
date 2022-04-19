7bb7388af8da4cf3c286cad269e39794f628e782
Jade-louise simm <Jade-louisesimm1991@outlook.com>
2022-04-19 09:49:50 +0100

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders h2', () => {
-  render(<App />);
+  render(<ToDoList />);
   const linkElement = screen.getByText(/Add Your to-do's to the list./);
   expect(linkElement).toBeInTheDocument();
 });
@@ -23,18 +23,18 @@ test("todo list header is rendered", () => {
 
 test("todo list header is rendered", () => {
   render(<App />)
-  let element = screen.getAllByText('delete')
+  let element = screen.getAllByText('To-Do list')
   expect(element).toBeInTheDocument()
 })
 
 test('input element is displayed ', () => {
-  render(<App />)
+  render(<ToDoForm />)
   let element = screen.getByPlaceholderText('Add a to-do')
   expect(element).toBeInTheDocument()
 })
 
 test('input element is displayed ', () => {
-  render(<App />)
+  render(<ToDoForm />)
   let element = screen.getByPlaceholderText('Update Your to-do')
   expect(element).toBeInTheDocument()
 })
